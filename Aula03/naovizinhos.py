3b57e61393138a95e23203ae6ea5af0d698007f1
pdlmachado <patricia@computacao.ufcg.edu.br>
2023-02-10 10:32:42 -0300

    splab

--- Aula03/naovizinhos.py
+++
@@ -1,8 +0,0 @@
-import networkx as nx
-from util.networkx_util import draw_graph
-
-def naovizinhos (G,v):
-    return [u for u in G.nodes if not u in G.neighbors(v) ]
-
-G = nx.read_graphml('graphs/m-u-cy-sc-p-01.graphml')
-print(G)
