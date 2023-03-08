0368a5567e60e292824501bdccd5762f379d7733
pdlmachado <patricia@computacao.ufcg.edu.br>
2023-03-08 08:35:24 -0300

    update

--- main.py
+++ main.py
@@ -5,9 +5,10 @@
 
 import networkx as nx
 from src.Q01 import associate_astronauts
+from src.Q02 import insep_blocks
 from util.networkx_util import read_multiple_CSV, draw_graph
 
-def manual_test_Q1():
+def example_Q1():
     a_list = [
         ('Kurtis','Shepstone','Italy'),
         ('Rafferty','Stoak','Canada'),
@@ -20,9 +21,6 @@ def manual_test_Q1():
         ('Eleanore','Pollett','USA'),
         ('Town','Duddell','Canada')
     ]
-    # G = nx.Graph()
-    # Criando os vértices do grafo a partir do arquivo CSV
-    # read_multiple_CSV(G, filename, vid="last_name")
     # Invocando a função
     G = associate_astronauts(a_list)
     print([G.nodes[n]['last_name'] for n in G.nodes])
@@ -33,9 +31,16 @@ def manual_test_Q1():
     # [('Shepstone', 'Stoak'), ('Shepstone', 'Anselm'), ('Shepstone', 'Caset'), ('Shepstone', 'Fearnyough'), ('Shepstone', 'Brazener'), ('Shepstone', 'Drohane'), ('Shepstone', 'Pollett'), ('Shepstone', 'Duddell'), ('Stoak', 'Thireau'), ('Stoak', 'Pollett'), ('Anselm', 'Thireau'), ('Anselm', 'Pollett'), ('Thireau', 'Caset'), ('Thireau', 'Fearnyough'), ('Thireau', 'Brazener'), ('Thireau', 'Drohane'), ('Thireau', 'Pollett'), ('Thireau', 'Duddell'), ('Caset', 'Pollett'), ('Fearnyough', 'Pollett'), ('Brazener', 'Pollett'), ('Drohane', 'Pollett'), ('Pollett', 'Duddell')]
     draw_graph(G, nx.circular_layout(G),node_labels=nx.get_node_attributes(G,'last_name'))
 
+def example_Q2(): 
+    G = nx.read_graphml('graphs/s-u-w-cy-sc-p-01.graphml')
+    limiar = 300
+    iblocks = insep_blocks(G,limiar)
+    print(iblocks)
+    draw_graph(G)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    manual_test_Q1()
-
+    #example_Q1()
+    example_Q2()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ src/Q02.py
@@ -0,0 +1,30 @@
+"""
+A administração de um município necessita agrupar os quarteirões de 
+uma cidade em distritos para facilitar sua gestão. Para tal, possui 
+um grafo onde os vértices representam quarteirões e arestas as 
+vizinhanças entre os quarteirões.
+Existem duas condições básicas a serem atendidas na divisão: 
+1) os quarteirões de um distrito têm que ser vizinhos; 
+2) Um grupo de 3 vizinhos mutuamente adjacentes não pode ser 
+separado em distritos diferentes, a não ser que a distância entre 
+dois deles seja maior que um limite de tolerância. 
+
+Considerando o exemplo do grafo abaixo, os quarteirões I,J,D não 
+podem ser separados se o limite de tolerância para a distância 
+for maior que 300. A distância entre dois quarteirões está 
+representada no peso da aresta.
+
+Construa uma função, **inseparable**, que recebe um grafo como 
+o apresentado no exemplo, um limite para tolerância de distância 
+entre quarteirões e retorna uma lista com os grupos de 3 
+quarteirões que não podem ser separados. 
+
+Dica: Use o conceito de incorporação do subgrafo K3 para 
+identificar os candidatos.
+"""
+
+import networkx as nx
+
+def insep_blocks (G,limiar):
+    list_cycles = nx.cycle_basis(G)
+    return list_cycles
